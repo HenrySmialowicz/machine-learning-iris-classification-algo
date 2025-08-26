097a5062be13a7f8d00bf5c3becb67c643e48514
Henry Smialowicz <henry.smialowicz@gmail.com>
2025-08-26 13:12:14 -0400

    Algo Evaluation w Dataset

--- IrisClassification.py
+++ IrisClassification.py
@@ -29,7 +29,7 @@ dataset = read_csv(url, names=names)
 #print(dataset.shape)
 
 #head
-print(dataset.head(20))
+#print(dataset.head(20))
 
 #descriptions
 #print(dataset.describe())
@@ -54,3 +54,29 @@ array = dataset.values
 X = array[:,0:4]
 y = array[:,4]
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+#Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LD', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+#evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Compare Algos
+plt.boxplot(results, labels=names)
+plt.title('Algorithm Comparison')
+plt.show()
+
+#SVC Algorithm Found to be most accurate
